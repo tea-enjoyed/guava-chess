1eff76ab58f44c0bfec283ec19a84a91e03ab647
tea-enjoyed <yurykup0927@gmail.com>
2025-06-01 15:46:33 -0400

    bug fixes + better UI + prevented spoofing & stuff

--- server/sockets/io.js
+++ server/sockets/io.js
@@ -1,13 +1,13 @@
-
+/* 
 
 module.exports = io => {
     io.on('connection', socket => {
-        console.log('New socket connection');
+        console.log('New player connected!');
 
         let currentCode = null;
 
         socket.on('move', function(move) {
-            console.log('move detected')
+            console.log('Move detected.')
 
             io.to(currentCode).emit('newMove', move);
         });
@@ -22,10 +22,11 @@ module.exports = io => {
             }
             
             io.to(currentCode).emit('startGame');
+            console.log("Game started!")
         });
 
         socket.on('disconnect', function() {
-            console.log('socket disconnected');
+            console.log('Player disconnected!');
 
             if (currentCode) {
                 io.to(currentCode).emit('gameOverDisconnect');
@@ -33,5 +34,55 @@ module.exports = io => {
             }
         });
 
+    });
+}; */
+
+module.exports = io => {
+    io.on('connection', socket => {
+        console.log('New player connected!');
+        let currentCode = null;
+        let redirected = false;
+
+        socket.on('joinGame', function(data) {
+            currentCode = data.code;
+
+            const room = io.sockets.adapter.rooms.get(currentCode);
+            const numPlayers = room ? room.size : 0;
+
+            if (numPlayers >= 2) {
+                socket.emit('redirectHome', { error: 'invalidCode' });
+                redirected = true;
+                return;
+            }
+
+            socket.join(currentCode);
+
+            if (numPlayers === 0) {
+                games[currentCode] = true;
+            }
+
+            if (numPlayers === 1) {
+                io.to(currentCode).emit('startGame');
+                console.log('Game started!');
+            }
+        });
+
+        socket.on('move', function(move) {
+            if (currentCode) {
+                io.to(currentCode).emit('newMove', move);
+            }
+        });
+
+        socket.on('disconnect', function() {
+            if (redirected) {
+                let redirected = false;
+                return;
+            }
+            console.log('Player disconnected!');
+            if (currentCode) {
+                io.to(currentCode).emit('gameOverDisconnect');
+                delete games[currentCode];
+            }
+        });
     });
 };
